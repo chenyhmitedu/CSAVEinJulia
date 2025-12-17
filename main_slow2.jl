2d1f69f02d60f4ec83e9d75e92ec16802f0bd67d
Henry Chen <chenyh@mit.edu>
2025-12-16 20:51:15 -0500

    Add slow model for comparison

---
+++ main_slow2.jl
@@ -0,0 +1,82 @@
+using Pkg
+Pkg.add(url="https://github.com/chenyhmitedu/CSVtoDIC")
+Pkg.add(url="https://github.com/chenyhmitedu/GTAPdata")
+Pkg.add("BenchmarkTools")
+Pkg.add("PATHSolver")
+Pkg.activate(".")
+Pkg.instantiate()
+
+using CSAVEinJulia
+using DataFrames
+using MPSGE
+using CSV
+using BenchmarkTools
+
+import PATHSolver
+PATHSolver.c_api_License_SetString("2830898829&Courtesy&&&USR&45321&5_1_2021&1000&PATH&GEN&31_12_2025&0_0_0&6000&0_0")
+
+data = load_gtap_data()
+
+# Vectors below may be changed depending on the sectoral names and resolution
+data["set_fe"]      = [:coa, :gas, :p_c]
+data["set_elec"]    = [:ely]
+data["set_ne"]      = setdiff(data["set_i"], union(data["set_fe"], data["set_elec"]))
+data["set_tr"]      = [:wtp, :atp, :otp]
+
+# Purposely put the CGE model here for comparison purposes
+
+include("./model_slow2.jl")
+solve!(MGE, cumulative_iteration_limit = 0)
+
+df_calib = generate_report(MGE)
+
+df_calib = df_calib[df_calib.margin .> 0.001, :]
+println(df_calib)
+
+set_silent(MGE)
+
+#==
+df = DataFrame(run = Int[], runtime = Float64[])
+N = 30
+for t ∈ 1:N
+    for i ∈ data["set_fe"], g ∈ data["set_g"]
+        #set_value!(MGE[:rtfd][i, g, :USA], data["rtfd0"][i, g, :USA]*2*(t-1)/(N-1))
+        #set_value!(MGE[:rtfi][i, g, :USA], data["rtfi0"][i, g, :USA]*2*(t-1)/(N-1))
+        set_value!(MGE[:rtfd][i, g, :USA], data["rtfd0"][i, g, :USA]*2)
+        set_value!(MGE[:rtfi][i, g, :USA], data["rtfi0"][i, g, :USA]*2)
+    end
+    runtime = @elapsed solve!(MGE; cumulative_iteration_limit = 1000, convergence_tolerance = 1e-8)
+    push!(df, (run = t, runtime = runtime))
+end
+println(df)
+==#
+
+for i ∈ data["set_fe"], g ∈ data["set_g"]
+    set_value!(MGE[:rtfd][i, g, :USA], data["rtfd0"][i, g, :USA]*2)
+    set_value!(MGE[:rtfi][i, g, :USA], data["rtfi0"][i, g, :USA]*2)
+end
+
+sol = @benchmarkable solve!(MGE; cumulative_iteration_limit = 1000, convergence_tolerance = 1e-8) samples = 1000 evals = 1 seconds = 18000
+model_sol = run(sol)
+sol_times_seconds = model_sol.times ./ 1_000_000_000
+df_sol_time = DataFrame(:sec => sol_times_seconds)
+
+path = joinpath(@__DIR__, "src/results/", "56x2_1000_sol_time_slow2.csv")
+CSV.write(path, df_sol_time)
+
+df_sol_time = generate_report(MGE)
+
+df_pf = filter(row -> startswith(string(row.var), "PF"), df_sol_time)
+pf = value.(MGE[:PF]) #Can just extract the one wanted variable this way
+
+df_filtered = df[df.margin .> 0.001, :]
+println(df_filtered)
+
+
+
+
+
+
+
+
+
